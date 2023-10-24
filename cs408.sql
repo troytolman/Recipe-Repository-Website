ece1bbaab31d7373ca3889008c83dd5c88f0c72b
troytolman <107224076+troytolman@users.noreply.github.com>
2023-10-24 17:06:07 -0600

    init

---
+++ cs408.sql
@@ -0,0 +1,32 @@
+use e1kh0ddh49syn5j4;
+
+CREATE TABLE users (
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    user_email VARCHAR(120) NOT NULL,
+    user_name VARCHAR(120) NOT NULL UNIQUE,
+    user_password VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE recipes (
+    recipeID INT PRIMARY KEY AUTO_INCREMENT,
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES users(userID),
+    title VARCHAR(140) NOT NULL,
+    cook_time INT NOT NULL,
+    serving_size INT NOT NULL,
+    ingredients VARCHAR(600) NOT NULL,
+    instructions VARCHAR(600) NOT NULL
+);
+
+CREATE TABLE favorites (
+    userID INT,
+    recipeID INT,
+    PRIMARY KEY (userID, recipeID),
+    FOREIGN KEY (userID) REFERENCES users(userID),
+    FOREIGN KEY (recipeID) REFERENCES recipes(recipeID)
+);
+
+-- Additional tables and features can be added as needed.
+
+
+
